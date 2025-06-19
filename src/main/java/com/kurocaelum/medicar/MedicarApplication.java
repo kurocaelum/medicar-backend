29e8afaf823f2d2e99ee0e61da35dcdc157b391b
Victor <victor_brandao@outlook.com>
2025-06-19 18:40:57 -0300

    first commit

---
+++ src/main/java/com/kurocaelum/medicar/MedicarApplication.java
@@ -0,0 +1,13 @@
+package com.kurocaelum.medicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicarApplication.class, args);
+	}
+
+}
