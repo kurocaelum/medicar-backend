ce3e9f75ddc93d23544ea7c1db1a8f818df6186a
Victor <victor_brandao@outlook.com>
2025-09-05 17:12:47 -0300

    feat - update, delete em services

--- src/main/java/com/kurocaelum/medicar/services/AgendaService.java
+++ src/main/java/com/kurocaelum/medicar/services/AgendaService.java
@@ -4,12 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.kurocaelum.medicar.dto.AgendaDTO;
 import com.kurocaelum.medicar.entities.Agenda;
 import com.kurocaelum.medicar.mappers.AgendaMapper;
 import com.kurocaelum.medicar.repositories.AgendaRepository;
+import com.kurocaelum.medicar.services.exceptions.DatabaseException;
+import com.kurocaelum.medicar.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class AgendaService {
@@ -38,4 +44,31 @@ public class AgendaService {
 		return repository.save(obj);
 	}
 	
+	public void delete(Long id) {
+		try {
+			if (!repository.existsById(id))
+				throw new ResourceNotFoundException(id);
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Agenda update(Long id, Agenda obj) {
+		try {
+			Agenda entity = repository.findById(id).get();
+			updateData(entity, obj);
+			
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(Agenda entity, Agenda obj) {
+		entity.setMedico(obj.getMedico());
+		entity.setDia(obj.getDia());
+	}
 }
--- src/main/java/com/kurocaelum/medicar/services/ConsultaService.java
+++ src/main/java/com/kurocaelum/medicar/services/ConsultaService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.kurocaelum.medicar.entities.Consulta;
 import com.kurocaelum.medicar.repositories.ConsultaRepository;
+import com.kurocaelum.medicar.services.exceptions.DatabaseException;
+import com.kurocaelum.medicar.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ConsultaService {
@@ -26,6 +32,35 @@ public class ConsultaService {
 	
 	public Consulta insert(Consulta obj) {
 		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			if (!repository.existsById(id))
+				throw new ResourceNotFoundException(id);
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Consulta update(Long id, Consulta obj) {
+		try {
+			Consulta entity = repository.findById(id).get();
+			updateData(entity, obj);
+			
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(Consulta entity, Consulta obj) {
+		entity.setHorario(obj.getHorario());
+		entity.setAgenda(obj.getAgenda());
+		entity.setDataAgendamento(obj.getDataAgendamento());
 	}
 	
 }
