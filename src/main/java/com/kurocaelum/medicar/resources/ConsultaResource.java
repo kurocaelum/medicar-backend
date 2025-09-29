46350634ff609909096806dd77fc39ecf4323170
Victor <victor_brandao@outlook.com>
2025-09-29 11:33:56 -0300

    refactor - endpoint de marcar consulta refatorado para PUT

--- src/main/java/com/kurocaelum/medicar/resources/ConsultaResource.java
+++ src/main/java/com/kurocaelum/medicar/resources/ConsultaResource.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,11 +51,11 @@ public class ConsultaResource {
 		
 		return ResponseEntity.noContent().build();
 	}
-	
-	@PostMapping
+
+	@PutMapping
 	public ResponseEntity<ConsultaDTO> marcarConsulta(@RequestBody ConsultaUpdateDTO obj) {
 		ConsultaDTO entity = service.marcarConsulta(obj);
-				
+		
 		return ResponseEntity.ok().body(entity);
 	}
 	
